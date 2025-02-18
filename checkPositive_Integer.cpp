b33ad0ab9edd49949363dd6e4e376bae7e93ff24
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-18 20:59:36 +0530

    -

---
+++ checkPositive_Integer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+  using namespace std;
+
+  int main()
+  {
+     int num;
+    cout << "Enter an Integer : ";
+    cin >> num;
+     
+    if (num > 0)
+    {
+        cout << "The entered integer is positive." << endl;
+    }
+    else if (num < 0)
+    {
+        cout << "The entered integer is negative." << endl;
+    }
+    else
+    {
+        cout << "Invalid Input" << endl;
+    }
+    return 0;
+  }
+  
