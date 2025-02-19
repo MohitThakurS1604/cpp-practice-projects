db96e52fcd2b4a082d6a4033f48623481a4cc4eb
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-19 17:34:45 +0530

    Basic C++ programs

---
+++ divide.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, divide;
+
+    cout << "Enter a number :";
+    cin >> num1;
+
+    cout << "Enter a number :";
+    cin >> num2;
+
+    divide = num1 / num2;
+
+    cout << "Division is :" << divide << endl;
+    return 0;
+}
---
+++ multiply.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, multiply;
+
+    cout << "Enter a number :";
+    cin >> num1;
+    cout << "Enter a number :";
+    cin >> num2;
+
+    multiply = num1 * num2;
+
+    cout << "Product is : " << multiply << endl;
+    return 0;
+}
