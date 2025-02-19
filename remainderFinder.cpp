22fe958e39368af84734b2e4d9ce9f51f5694d97
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-19 17:48:36 +0530

    Create remainderFinder.cpp

---
+++ remainderFinder.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+ using namespace std;
+  int main()
+  {
+    int num1,num2,remainder;
+
+    cout << "Enter a number :";
+    cin >> num1;
+    cout << "Enter a number :";
+    cin >> num2;
+
+    remainder = num1 5 num2
+
+    cout <<"Remainder is : "<< remainder <<endl;
+    return 0;
+  }
