4ff960d9838475b6872f373c2d266675cb7eaecf
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-19 17:40:00 +0530

    C++ program to subtaract two numbers

---
+++ subtraction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1, num2, subtraction;
+
+    cout << "Enter a number :";
+    cin >> num1;
+    cout << "Enter a number :";
+    cin >> num2;
+
+    subtraction = num1 - num2;
+
+    cout << "Subtraction is : " << subtraction << endl;
+    return 0;
+}
