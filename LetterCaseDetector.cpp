351d50b59a9221a70f2ab34bb197e13cac1c66da
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-20 22:50:17 +0530

    Create LetterCaseDetector.cpp

---
+++ LetterCaseDetector.cpp
@@ -0,0 +1,25 @@
+/*
+  
+C++ program to check that the entered number is uppercase or lowercase
+  
+  */
+
+  #include"iostream"
+   using namespace std;
+
+   int main()
+   {
+     char input;
+
+     cout << "Emter any character : " <<  endl;
+     cin >> input;
+
+     if (input >= 'a' && input <= 'z')
+     {
+       cout << "The entered character is a lowercase character " << endl;
+     }
+       else {
+
+           cout << "The entered character is a uppercase character " << endl;
+       }
+   }
