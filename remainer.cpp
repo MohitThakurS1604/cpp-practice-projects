c1987419cfcddf051977e562011d4a69c71a8c14
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-21 21:26:43 +0530

    Update remainer.cpp

--- remainer.cpp
+++ remainer.cpp
@@ -1,2 +1,14 @@
-#include<iostream>
-  using namespace std;
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, remainder;
+    cout << "Enter a number :";
+    cin >> num1;
+    cout << "Enter a number :";
+    cin >> num2;
+    remainder = num1 % num2;
+    cout << "Remainder is : " << remainder << endl;
+    return 0;
+}
