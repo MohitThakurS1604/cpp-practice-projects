40e108137bb7a0074a601a9852897c8af9aedb9c
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-22 22:47:11 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,20 +1,25 @@
 /*
-*/
+
+ C++ program to find factorial of nny number using while loop
+
+ */
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int num, factorial = 1;
     cout << "Enter a number: ";
     cin >> num;
-    
+
     int i = num;
-    while (i > 0) {
+    while (i > 0)
+    {
         factorial *= i;
         i--;
     }
-    
+
     cout << "Factorial: " << factorial << endl;
     return 0;
 }
