7b4638930ddecf7f1b94bb8b72b410e3ee10e45d
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-22 22:41:21 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, factorial = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    int i = num;
+    while (i > 0) {
+        factorial *= i;
+        i--;
+    }
+    
+    cout << "Factorial: " << factorial << endl;
+    return 0;
+}
