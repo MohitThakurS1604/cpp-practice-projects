736bb0ea1973c7af6e6be60fde9d5d0247aafc50
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-02-23 12:56:13 +0530

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,23 +1,15 @@
 /*
 
-   C+++ program to rpint hello world
+   C+++ program to print hello world
    
 */
 
 #include <iostream>
 
 int main() {
-    int num1, num2, sum;
-
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
-
-    sum = num1 + num2;
-
-    std::cout << "The sum is: " << sum << std::endl;
+  
+    using namespace std;
+    cout << "Hello, World!" << endl;
 
     return 0;
 }
