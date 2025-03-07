54e4a56f1d4a093cacd6888ae54611ea87e91b00
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-07 11:34:01 +0530

    Create CarDetails.cpp

---
+++ CarDetails.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+private:
+    string brand;
+    string model;
+    int year;
+
+public:
+    void setCarDetails(string b, string m, int y) {
+        brand = b;
+        model = m;
+        year = y;
+    }
+
+    void displayCarDetails() {
+        cout << "Car Details:\n";
+        cout << "Brand: " << brand << "\nModel: " << model << "\nYear: " << year << endl;
+    }
+};
+
+int main() {
+    Car myCar;
+    myCar.setCarDetails("Toyota", "Camry", 2022);
+    myCar.displayCarDetails();
+    return 0;
+}
