29ee9f495cbb4ef46f3ffa1f280297760a72e7d1
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-07 11:37:18 +0530

    Create RectangleCalculator.cpp

---
+++ RectangleCalculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    void setDimensions(double l, double w) {
+        length = l;
+        width = w;
+    }
+
+    double calculateArea() {
+        return length * width;
+    }
+
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+};
+
+int main() {
+    Rectangle rect;
+    rect.setDimensions(10.5, 5.3);
+    cout << "Area: " << rect.calculateArea() << endl;
+    cout << "Perimeter: " << rect.calculatePerimeter() << endl;
+    return 0;
+}
