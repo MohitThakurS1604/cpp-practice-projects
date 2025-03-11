0d6d54534f3c679fa5d33e26b0ee4e928077b6c4
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-11 10:18:01 +0530

    Create friend_function.cpp

---
+++ friend_function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+private:
+    int data;
+
+public:
+    MyClass(int val) : data(val) {}
+
+    // Declaring friend function
+    friend void display(const MyClass& obj);
+};
+
+// Friend function definition (outside the class)
+void display(const MyClass& obj) {
+    cout << "Data: " << obj.data << endl;  // Accessing private member
+}
+
+int main() {
+    MyClass obj(10);
+    display(obj);  // Calling the  friend function
+
+    return 0;
+}
