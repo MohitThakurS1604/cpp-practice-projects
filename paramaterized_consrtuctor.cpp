10fdd7c9a54133943616364f0507737454671f0d
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-18 10:06:03 +0530

    Create paramaterized_consrtuctor.cpp

---
+++ paramaterized_consrtuctor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Number {
+public:
+    int value;
+
+    // parameterized constructor
+    Number(int v) {
+        value = v;
+    }
+};
+
+int main() {
+    Number num(10);
+    cout << "Value: " << num.value << endl;
+    return 0;
+}
