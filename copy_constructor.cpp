e6cf4494a85851e9b743d4e86d7ecb2d099e673a
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-18 10:07:25 +0530

    Create copy_constructor.cpp

---
+++ copy_constructor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Box {
+public:
+    int size;
+
+    // Parameterized constructor
+    Box(int s) {
+        size = s;
+    }
+
+    // Copy constructor
+    Box(const Box &b) {
+        size = b.size;
+    }
+};
+
+int main() {
+    Box box1(5);         // Original object
+    Box box2 = box1;     // Copy using copy constructor
+
+    cout << "Box1 size: " << box1.size << endl;
+    cout << "Box2 size: " << box2.size << endl;
+
+    return 0;
+}
