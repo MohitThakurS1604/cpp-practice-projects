08989543960a728d35c62c6db77ca8f5fd7fc7ae
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-03-18 10:06:08 +0530

    Update oddSum.cpp

--- oddSum.cpp
+++ oddSum.cpp
@@ -1,20 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+class MyClass {
+public:
+    int number;
 
-    int num,sum=0;
-     cout << "enter a number :";
-     cin >> num;
+    // Default constructor
+    MyClass() {
+        number = 0;
+    }
+};
 
-     for (int i = 1; i <= num; i++) // Changed condition to i <= num
-     {
-        if ( i % 2 !=0)
-         {
-            sum += i;
-         }
-     }
-        cout << sum << endl;
-         return 0;
+int main() {
+    MyClass obj;
+    cout << "Number: " << obj.number << endl;
+    return 0;
 }
