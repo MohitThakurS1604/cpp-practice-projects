12f59bf82e45e953e2db7107c35c52f16c2edf3e
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-04-04 11:48:45 +0530

    Create parameterOverloadingNumbers.cpp

---
+++ parameterOverloadingNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Function with 1 parameter
+void show(int x) {
+    cout << "Value: " << x << endl;
+}
+
+// Function with 2 parameters
+void show(int x, int y) {
+    cout << "Sum: " << x + y << endl;
+}
+
+// Function with 3 parameters
+void show(int x, int y, int z) {
+    cout << "Product: " << x * y * z << endl;
+}
+
+int main() {
+    show(5);             // Calls function with 1 parameter
+    show(3, 4);          // Calls function with 2 parameters
+    show(2, 3, 4);       // Calls function with 3 parameters
+
+    return 0;
+}
